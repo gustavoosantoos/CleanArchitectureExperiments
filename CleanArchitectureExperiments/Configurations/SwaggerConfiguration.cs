5fd65ae432227701577bc817da24790f6e619652
Gustavo Oliveira <gustavo.dev@outlook.com.br>
2019-04-06 12:20:38 -0300

    Alterações para melhorias de performance

---
+++ CleanArchitectureExperiments/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitectureExperiments.Configurations
+{
+    public static class SwaggerConfiguration
+    {
+        public static void AddApiSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerDocument();
+        }
+
+        public static void UseApiSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUi3();
+        }
+    }
+}
